f44a677e469b6c4e25d09739669e7c9952edb780
IsItIzzet <ohlhjames@gmail.com>
2024-02-21 16:18:21 -0600

    added functioanlity to the calculator so that users can see the calculated message displayed on the screen without needing to use the console

---
+++ script.js
@@ -0,0 +1,104 @@
+
+const path = document.getElementById('character');
+const abundance = ["Gallagher","Huohuo", "Luocha", "Natasha", "Bailu", "Lynx"];
+const destruction = ["Xiyue", "Arlan", "Blade", "Clara", "Hook", "Dan Heng IL", "Jing Liu", "Physical MC"]
+const erudition = ["Herta", "Himeko", "Jing Yuan", "Serval", "Qingque", "Argenti"];
+const harmony = ["Asta", "Tingyun", "Bronya", "Hanya", "Ruan Mei"];
+const hunt = ["Dr. Ratio", "Seele", "Dan Heng", "Sushang", "Topaz", "Topaz and Numby", "Yanqing"]
+const nihility = ["Luka", "Kafka", "Sampo", "Pela", "Silver Wolf", "Welt"];
+const preservation = ["Aventurine","Gepard", "March 7th", "Fire MC", "Fu Xuan"];
+const nihility2 = ["Acheron","Black Swan"]
+const destruction2 = ["Misha"]
+const harmony2 =["Sparkle"]
+const personalNeedP = document.getElementById('needed-mats-p');              
+const trailblaze_hand = document.getElementById('current-trailblaze-p');
+const button = document.getElementById("button");
+const message = document.getElementById("message");
+let pathMat = "";
+let totalNeedP = 0;
+let totalNeedB = 0;
+let totalNeedG = 0;
+let iterations = 0;
+let randomGr= 0;
+let randomBl = 0;
+let randomPu = 0;
+let hsrRun = function(event){
+    event.preventDefault();
+    if(abundance.includes(path.value)){
+        pathMat = "Flowers of Eternity";
+    }
+    if(destruction.includes(path.value)){
+        pathMat = "Worldbreaker Blade";
+    }
+    if(erudition.includes(path.value)){
+        pathMat = "Key of Wisdom";
+    }
+    if(hunt.includes(path.value)){
+        pathMat = "Arrow of the Starchaser";
+    }
+    if(harmony.includes(path.value)){
+        pathMat = "Stellaris Symphony";
+    }
+    if(nihility.includes(path.value)){
+        pathMat = "Obsidian of Obsession";
+    }
+    if(preservation.includes(path.value)){
+        pathMat = "Safeguard of Amber";
+    }
+    if(nihility2.includes(path.value)){
+        pathMat = "Heaven Incinerator"
+    }
+    if(harmony2.includes(path.value)){
+        pathMat = "Heavenly Melody"
+    }
+    if(destruction2.includes(path.value)){
+        pathMat = "Moon Madness Fang"
+    }
+    hsrCalc();
+}
+let messageSet = function(iterations){   
+    message.textContent = "This would take approximately: " + iterations + " runs to earn " + personalNeedP.value + " of " + pathMat + " for " + path.value + ".";
+}
+let hsrCalc = function(){  
+    let totalNeedP = 0;
+    let iterations = 0;
+    let randomGr= 0;
+    let randomBl = 0;
+    let randomPu = 0; 
+    while (totalNeedP <= personalNeedP.value){
+        randomGr += (Math.floor(Math.random() * 3) + 6);
+        randomBl += (Math.floor(Math.random() * 3) + 6);
+        randomPu += (Math.floor(Math.random() * 3));
+        console.log(randomGr, randomBl, randomPu)
+        if (randomGr % 3 == 0){
+            randomBl += randomGr / 3;
+            randomGr = 0;
+            if (randomBl % 3 ==0){
+                randomPu += randomBl / 3;
+                randomBl = 0;
+            }
+        }       
+        totalNeedP += randomPu;
+        iterations += 1;
+    }
+    messageSet(iterations);
+    let trailblaze_use = iterations * 60
+    trailblaze_use -= trailblaze_hand.value
+    if (trailblaze_use > 0){
+        message.textContent += " This would take approximately: " + trailblaze_use + " more Trailblaze power to accomplish.";
+        trailblaze_use *= 6
+        if (trailblaze_use >= 60){
+            let trailblaze_remainder = trailblaze_use % 60
+            trailblaze_use -= trailblaze_remainder
+            trailblaze_use /= 60
+            if (trailblaze_remainder == 0){
+                message.textContent += " It will take: " + trailblaze_use + " hours to accrue the amount of trailblaze necessary to run this many Calyx's.";
+            }else{
+                message.textContent +=  " It will take: " + trailblaze_use + " hours and " + trailblaze_remainder + " minutes to accrue the amount of trailblaze necessary to run this many Calyx's.";
+            } 
+        }      
+    }else{
+        message.textContent += "You have enough Trailblaze power to conduct that amount of runs!";
+    }
+}
+button.addEventListener('click', hsrRun);
